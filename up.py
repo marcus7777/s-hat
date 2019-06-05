2b54b5db970baef99195cca9c4fc924917125893
Marcus Hitchins <marcus7777@gmail.com>
2019-06-06 00:28:44 +0100

    Update up.py

--- up.py
+++ up.py
@@ -166,7 +166,8 @@ def play(user_input,inputs):
           if rodX <= 6:
             if maze[rodY][rodX + 1] == 1:
               rodX += 1
-      elif rodX > 0 and state == "Maze" and rodX == saveX and rodY == saveY:
+      
+      if rodX > 0 and state == "Maze" and rodX == saveX and rodY == saveY:
         state = "End Game"
     if state == 'Startup':
       sleep(.2)
